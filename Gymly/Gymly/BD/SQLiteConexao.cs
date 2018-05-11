f5ef812ac73d6be3f1ecbc736ec06e0ebd99caea
Felipe M <felipenuccimorassutti@gmail.com>
2018-05-10 22:12:58 -0300

    tabelas de anamnese e av.fisica foram criadas. Removemos Angina da tela da Anamnese. Corrigi o texto de Avaliação Fisica

--- Gymly/Gymly/BD/SQLiteConexao.cs
+++ Gymly/Gymly/BD/SQLiteConexao.cs
@@ -54,10 +54,96 @@ namespace Gymly.BD
                 cmd = new SQLiteCommand(sql.ToString(), conn);
                 cmd.ExecuteNonQuery();
                 sql.Clear();
+
+
+
                 sql.AppendLine("CREATE TABLE IF NOT EXISTS ANAMNESES ([ID] INTEGER PRIMARY KEY AUTOINCREMENT,");
-                sql.AppendLine("[ALTURA] DOUBLE);");
+                sql.AppendLine("[ID] INT");
+                sql.AppendLine("[ID_ALUNO] INT,");
+                sql.AppendLine("[HISTORICO_PROBLEMA_CARDIACO] INT,");
+                sql.AppendLine("[HISTORICO_DORES_PEITO] INT,");
+                sql.AppendLine("[HISTORICO_DESMAIOS_OU_VERTIGEM] INT,");
+                sql.AppendLine("[HISTORICO_PRESSAO_ALTA] INT,");
+                sql.AppendLine("[HISTORICO_PROBLEMA_OSSEO] INT,");
+                sql.AppendLine("[RAZAO_FISICA] INT,");
+                sql.AppendLine("[IDOSO_REGULAR] INT,");
+                sql.AppendLine("[DOENCA_CARDIACA_CORONARIANA] INT,");
+                sql.AppendLine("[DOENCA_CARDIACA_REUMATICA] INT,");
+                sql.AppendLine("[DOENCA_CARDIACA_CONGENITA] INT,");
+                sql.AppendLine("[BAIMENTOS_CARDIACOS_IRREGULARES] INT,");
+                sql.AppendLine("[PROBLEMA_VALVULAS_CARDIACAS] INT,");
+                sql.AppendLine("[MURMURIOS_CARDIACOS] INT,");
+                sql.AppendLine("[ATAQUE_CARDIACO] INT,");
+                sql.AppendLine("[DERRAME_CEREBRAL] INT,");
+                sql.AppendLine("[EPILEPSIA] INT,");
+                sql.AppendLine("[DIABETES] INT,");
+                sql.AppendLine("[HIPERTENSAO] INT,");
+                sql.AppendLine("[CANCER] INT,");
+                //AINDA NÃO FORAM IMPLEMENTADOS NA TELA
+                sql.AppendLine("[DOR_COSTAS] INT,");
+                sql.AppendLine("[DOR_ARTICULACAO] INT,");
+                sql.AppendLine("[DOR_PULMONAR] INT,");
+                sql.AppendLine("[PROBLEMA_RECENTE_PARENTE] INT,");
+                sql.AppendLine("[RESTRICAO_FISICA] INT,");
+                sql.AppendLine("[GESTANTE] INT,");
+                sql.AppendLine("[FUMANTE] INT,");
+                sql.AppendLine("[BEBIDA_ALCOOLICA] INT,");
+                sql.AppendLine("[TODOS_GRUPOS_ALIMENTARES] INT,");
+                sql.AppendLine("[ALTA_GORDURA_SATURADA] INT,");
+                sql.AppendLine("[PERDER_PESO] INT,");
+                sql.AppendLine("[MELHORAR_FLEXIBILIDADE] INT,");
+                sql.AppendLine("[PARAR_FUMAR] INT,");
+                sql.AppendLine("[REDUZIR_DORES_COSTAS] INT,");
+                sql.AppendLine("[MELHORAR_NUTRICAO] INT,");
+                sql.AppendLine("[MELHORAR_APTIDAO] INT,");
+                sql.AppendLine("[MELHORAR_MUSCULAR] INT,");
+                sql.AppendLine("[REDUZIR_ESTRESSE] INT,");
+                sql.AppendLine("[DIMINUIR_COLESTEROL] INT,");
+                sql.AppendLine("[SENTIR_MELHOR] INT,");
+
+
+                sql.AppendLine("FOREIGN KEY(ID_ALUNO) REFERENCES ALUNO(ID));");
+
+
                 cmd = new SQLiteCommand(sql.ToString(), conn);
                 cmd.ExecuteNonQuery();
+                sql.Clear();
+
+                
+                sql.AppendLine("CREATE TABLE IF NOT EXISTS AVALIACAO FISICA ([ID] INTEGER PRIMARY KEY AUTOINCREMENT,");
+                sql.AppendLine("[ID] INT,");
+                sql.AppendLine("[CPF_ALUNO] INT,");
+                sql.AppendLine("[DATA] DATE,");
+                sql.AppendLine("[IDADE] INT,");
+                sql.AppendLine("[PESO] FLOAT,");
+                sql.AppendLine("[ALTURA] FLOAT,");
+                sql.AppendLine("[TAXA_MUSCULO] FLOAT,");
+                sql.AppendLine("[TAXA_GORDURA] FLOAT,");
+                sql.AppendLine("[TAXA_AGUA] FLOAT,");
+                sql.AppendLine("[FREQUENCIA_CARDIACA] INT,");
+                sql.AppendLine("[PRESSAO_ARTERIAL] VARCHAR(7),");
+                sql.AppendLine("[BRACO_DIREITO] FLOAT,");
+                sql.AppendLine("[BRACO_ESQUERDO] FLOAT,");
+                sql.AppendLine("[ANTEBRACO_DIREITO] FLOAT,");
+                sql.AppendLine("[ANTEBRACO_ESQUERDO] FLOAT,");
+                sql.AppendLine("[TORAX] FLOAT,");
+                sql.AppendLine("[CINTURA] FLOAT,");
+                sql.AppendLine("[ABDOME] FLOAT,");
+                sql.AppendLine("[QUADRIL] FLOAT,");
+                sql.AppendLine("[COXA_DIREITA] FLOAT,");
+                sql.AppendLine("[COXA_ESQUERDA] FLOAT,");
+                sql.AppendLine("[PANTURRILHA_DIREITA] FLOAT,");
+                sql.AppendLine("[PANTURRILHA_ESQUERDA] FLOAT,");
+                sql.AppendLine("[FLEXAO] INT,");
+                sql.AppendLine("[ABDOMINAL] INT,");
+
+                sql.AppendLine("FOREIGN KEY(CPF_ALUNO) REFERENCES ALUNOS(CPF));");
+
+                cmd = new SQLiteCommand(sql.ToString(), conn);
+                cmd.ExecuteNonQuery();
+
+
+
                 conn.Close();
             }
         }
