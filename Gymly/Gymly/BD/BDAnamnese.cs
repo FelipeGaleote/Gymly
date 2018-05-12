a0d34ca312ad9cca548244e435475554471110eb
Felipe M <felipe.morassutti@hotmail.com>
2018-05-12 01:11:32 -0300

    Fiz a classe Anamnese e método insereAnamnese no BD

--- Gymly/Gymly/BD/BDAnamnese.cs
+++ Gymly/Gymly/BD/BDAnamnese.cs
@@ -11,7 +11,7 @@ namespace Gymly.BD
     class BDAnamnese
     {
         
-        public void insereAnamnese(Anamnese anamnese)
+        public void insereAnamnese()
         {
             SQLiteConexao conexao = new SQLiteConexao();
             SQLiteConnection conn = conexao.getConexao();
@@ -23,6 +23,64 @@ namespace Gymly.BD
             cmd.ExecuteNonQuery();
             conn.Close();
             sql.Clear();
+        }
+        public void insereAnamnese(Anamnese anamnese, int idaluno)
+        {
+            SQLiteConexao conexao = new SQLiteConexao();
+            SQLiteConnection conn = conexao.getConexao();
+            string sql = "INSERT INTO Anamnese(historico_problema_cardiaco,historico_dores_peito,historico_desmaios_ou_vertigem,historico_pressao_alta,historico_problema_osseo,razao_fisica," +
+                "idoso_regular,doenca_cardiaca_coronariana,doenca_cardiaca_reumatica,doenca_cardiaca_congenita,batimentos_cardiacos_irregulares,problema_valvulas_cardiacas,murmurios_cardiacos," +
+                "ataque_cardiaco,derrame_cerebral,epilepsia,diabetes,hipertensao,cancer,dor_articulacao,dor_pulmonar," +"problema_recente_parente,restricao_fisica,gestante,fumante,bebida_alcoolica," +
+               "todos_grupos_alimentares,alta_gordura_saturada,perder_peso,parar_fumar,reduzir_dores_costas,melhorar_nutricao,melhorar_aptidao,melhorar_muscular,reduzir_estresse,diminuir_colesterol," + "sentir_melhor) " +
+                "VALUES(@historico_problema_cardiaco,@historico_dores_peito,@historico_desmaios_ou_vertigem,@historico_pressao_alta,@historico_problema_osseo," +
+                "@razao_fisica,@idoso_regular,@doenca_cardiaca_coronariana,@doenca_cardiaca_reumatica,@doenca_cardiaca_congenita,@batimentos_cardiacos_irregulares," +
+                "@problema_valvulas_cardiacas,@murmurios_cardiacos,@ataque_cardiaco,@derrame_cerebral,@epilepsia,@diabetes,@hipertensao,@cancer,@dor_articulacao,@dor_pulmonar," +
+                "@problema_recente_parente,@restricao_fisica,@gestante,@fumante,@bebida_alcoolica,@todos_grupos_alimentares,@alta_gordura_saturada,@perder_peso,@parar_fumar," +
+                "@reduzir_dores_costas,@melhorar_nutricao,@melhorar_aptidao,@melhorar_muscular,@reduzir_estresse,@diminuir_colesterol,@sentir_melhor)";
+
+            SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+            cmd.Parameters.AddWithValue("idaluno", idaluno);
+            cmd.Parameters.AddWithValue("historico_problema_cardiaco", anamnese.Historico_problema_cardiaco);
+            cmd.Parameters.AddWithValue("historico_dores_peito", anamnese.Historico_dores_peito);
+            cmd.Parameters.AddWithValue("historico_desmaios_ou_vertigem", anamnese.Historico_desmaios_ou_vertigem);
+            cmd.Parameters.AddWithValue("historico_pressao_alta" , anamnese.Historico_pressao_alta);
+            cmd.Parameters.AddWithValue("historico_problema_osseo", anamnese.Historico_problema_osseo);
+            cmd.Parameters.AddWithValue("razao_fisica", anamnese.Razao_fisica);
+            cmd.Parameters.AddWithValue("idoso_regular", anamnese.Idoso_regular);
+            cmd.Parameters.AddWithValue("doenca_cardiaca_coronariana", anamnese.Doenca_cardiaca_coronariana);
+            cmd.Parameters.AddWithValue("doenca_cardiaca_reumatica", anamnese.Doenca_cardiaca_reumatica);
+            cmd.Parameters.AddWithValue("doenca_cardiaca_congenita", anamnese.Doenca_cardiaca_congenita);
+            cmd.Parameters.AddWithValue("batimentos_cardiacos_irregulares", anamnese.Batimentos_cardiacos_irregulares);
+            cmd.Parameters.AddWithValue(" problema_valvulas_cardiacas", anamnese.Problema_valvulas_cardiacas);
+            cmd.Parameters.AddWithValue("murmurios_cardiacos", anamnese.Murmurios_cardiacos);
+            cmd.Parameters.AddWithValue("ataque_cardiaco", anamnese.Ataque_cardiaco);
+            cmd.Parameters.AddWithValue("derrame_cerebral", anamnese.Derrame_cerebral);
+            cmd.Parameters.AddWithValue("epilepsia", anamnese.Epilepsia);
+            cmd.Parameters.AddWithValue("diabetes", anamnese.Diabetes);
+            cmd.Parameters.AddWithValue("hipertensao", anamnese.Hipertensao);
+            cmd.Parameters.AddWithValue("cancer", anamnese.Cancer);
+            cmd.Parameters.AddWithValue("dor_articulacao", anamnese.Dor_articulacao);
+            cmd.Parameters.AddWithValue("dor_pulmonar", anamnese.Dor_pulmonar);
+            cmd.Parameters.AddWithValue("problema_recente_parente", anamnese.Problema_recente_parente);
+            cmd.Parameters.AddWithValue("restricao_fisica", anamnese.Restricao_fisica);
+            cmd.Parameters.AddWithValue("gestante", anamnese.Gestante);
+            cmd.Parameters.AddWithValue("fumante", anamnese.Fumante);
+            cmd.Parameters.AddWithValue("bebida_alcoolica", anamnese.Bebida_alcoolica);
+            cmd.Parameters.AddWithValue("todos_grupos_alimentares", anamnese.Todos_grupos_alimentares);
+            cmd.Parameters.AddWithValue("alta_gordura_saturada", anamnese.Alta_gordura_saturada);
+            cmd.Parameters.AddWithValue("perder_peso", anamnese.Perder_peso);
+            cmd.Parameters.AddWithValue("parar_fumar", anamnese.Parar_fumar);
+            cmd.Parameters.AddWithValue("reduzir_dores_costas", anamnese.Reduzir_dores_costas);
+            cmd.Parameters.AddWithValue("melhorar_nutricao", anamnese.Melhorar_nutricao);
+            cmd.Parameters.AddWithValue("melhorar_aptidao", anamnese.Melhorar_aptidao);
+            cmd.Parameters.AddWithValue("melhorar_muscular", anamnese.Melhorar_muscular);
+            cmd.Parameters.AddWithValue("reduzir_estresse", anamnese.Reduzir_estresse);
+            cmd.Parameters.AddWithValue("diminuir_colesterol", anamnese.Diminuir_colesterol);
+            cmd.Parameters.AddWithValue("sentir_melhor", anamnese.Sentir_melhor);
+
+            
+            cmd.ExecuteNonQuery();
+            conn.Close();
         }
     }
 }
--- Gymly/Gymly/Models/Anamnese.cs
+++ Gymly/Gymly/Models/Anamnese.cs
@@ -6,7 +6,87 @@ using System.Threading.Tasks;
 
 namespace Gymly.Models
 {
-    class Anamnese
-    {
+    public class Anamnese { 
+        private int id;
+
+        private bool historico_problema_cardiaco;
+        private bool historico_dores_peito;
+        private bool historico_desmaios_ou_vertigem;
+        private bool historico_pressao_alta;
+        private bool historico_problema_osseo;
+        private bool razao_fisica;
+        private bool idoso_regular;
+        private bool doenca_cardiaca_coronariana;
+        private bool doenca_cardiaca_reumatica;
+        private bool doenca_cardiaca_congenita;
+        private bool batimentos_cardiacos_irregulares;
+        private bool problema_valvulas_cardiacas;
+        private bool murmurios_cardiacos;
+        private bool ataque_cardiaco;
+        private bool derrame_cerebral;
+        private bool epilepsia;
+        private bool diabetes;
+        private bool hipertensao;
+        private bool cancer;
+        private bool dor_articulacao;
+        private bool dor_pulmonar;
+        private bool problema_recente_parente;
+        private bool restricao_fisica;
+        private bool gestante;
+        private bool fumante;
+        private bool bebida_alcoolica;
+        private bool todos_grupos_alimentares;
+        private bool alta_gordura_saturada;
+        private bool perder_peso;
+        private bool parar_fumar;
+        private bool reduzir_dores_costas;
+        private bool melhorar_nutricao;
+        private bool melhorar_aptidao;
+        private bool melhorar_muscular;
+        private bool reduzir_estresse;
+        private bool diminuir_colesterol;
+        private bool sentir_melhor;
+
+
+
+        public int Id { get => id; set => id = value; }
+        public bool Historico_problema_cardiaco { get => historico_problema_cardiaco; set => historico_problema_cardiaco = value; }
+        public bool Historico_dores_peito { get => historico_dores_peito; set => historico_dores_peito = value; }
+        public bool Historico_desmaios_ou_vertigem { get => historico_desmaios_ou_vertigem; set => historico_desmaios_ou_vertigem = value; }
+        public bool Historico_pressao_alta { get => historico_pressao_alta; set => historico_pressao_alta = value; }
+        public bool Historico_problema_osseo { get => historico_problema_osseo; set => historico_problema_osseo = value; }
+        public bool Razao_fisica { get => razao_fisica; set => razao_fisica = value; }
+        public bool Idoso_regular { get => idoso_regular; set => idoso_regular = value; }
+        public bool Doenca_cardiaca_coronariana { get => doenca_cardiaca_coronariana; set => doenca_cardiaca_coronariana = value; }
+        public bool Doenca_cardiaca_reumatica { get => doenca_cardiaca_reumatica; set => doenca_cardiaca_reumatica = value; }
+        public bool Doenca_cardiaca_congenita { get => doenca_cardiaca_congenita; set => doenca_cardiaca_congenita = value; }
+        public bool Batimentos_cardiacos_irregulares { get => batimentos_cardiacos_irregulares; set => batimentos_cardiacos_irregulares = value; }
+        public bool Problema_valvulas_cardiacas { get => problema_valvulas_cardiacas; set => problema_valvulas_cardiacas = value; }
+        public bool Murmurios_cardiacos { get => murmurios_cardiacos; set => murmurios_cardiacos = value; }
+        public bool Ataque_cardiaco { get => ataque_cardiaco; set => ataque_cardiaco = value; }
+        public bool Derrame_cerebral { get => derrame_cerebral; set => derrame_cerebral = value; }
+        public bool Epilepsia { get => epilepsia; set => epilepsia = value; }
+        public bool Diabetes { get => diabetes; set => diabetes = value; }
+        public bool Hipertensao { get => hipertensao; set => hipertensao = value; }
+        public bool Cancer { get => cancer; set => cancer = value; }
+        public bool Dor_articulacao { get => dor_articulacao; set => dor_articulacao = value; }
+        public bool Dor_pulmonar { get => dor_pulmonar; set => dor_pulmonar = value; }
+        public bool Problema_recente_parente { get => problema_recente_parente; set => problema_recente_parente = value; }
+        public bool Restricao_fisica { get => restricao_fisica; set => restricao_fisica = value; }
+        public bool Gestante { get => gestante; set => gestante = value; }
+        public bool Fumante { get => fumante; set => fumante = value; }
+        public bool Bebida_alcoolica { get => bebida_alcoolica; set => bebida_alcoolica = value; }
+        public bool Todos_grupos_alimentares { get => todos_grupos_alimentares; set => todos_grupos_alimentares = value; }
+        public bool Alta_gordura_saturada { get => alta_gordura_saturada; set => alta_gordura_saturada = value; }
+        public bool Perder_peso { get => perder_peso; set => perder_peso = value; }
+        public bool Parar_fumar { get => parar_fumar; set => parar_fumar = value; }
+        public bool Reduzir_dores_costas { get => reduzir_dores_costas; set => reduzir_dores_costas = value; }
+        public bool Melhorar_nutricao { get => melhorar_nutricao; set => melhorar_nutricao = value; }
+        public bool Melhorar_aptidao { get => melhorar_aptidao; set => melhorar_aptidao = value; }
+        public bool Melhorar_muscular { get => melhorar_muscular; set => melhorar_muscular = value; }
+        public bool Reduzir_estresse { get => reduzir_estresse; set => reduzir_estresse = value; }
+        public bool Diminuir_colesterol { get => diminuir_colesterol; set => diminuir_colesterol = value; }
+        public bool Sentir_melhor { get => sentir_melhor; set => sentir_melhor = value; }
+
     }
 }
