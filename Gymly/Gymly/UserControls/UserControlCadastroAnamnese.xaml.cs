c526777df48bb3a521486c5e89c0866d14fffd48
Felipe Galeote <felipe10gale@gmail.com>
2018-06-16 04:38:20 -0300

    Remoção de imports desnecessarios em todos usercontrols.

--- Gymly/Gymly/UserControls/UserControlCadastroAnamnese.xaml.cs
+++ Gymly/Gymly/UserControls/UserControlCadastroAnamnese.xaml.cs
@@ -1,20 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Gymly.Models;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Gymly.BD;
-using Gymly.Models;
-using Gymly.UserControls;
 
 namespace Gymly.UserControls
 {
--- Gymly/Gymly/UserControls/UserControlCadastroAnamneseProximaEtapaFinal.xaml.cs
+++ Gymly/Gymly/UserControls/UserControlCadastroAnamneseProximaEtapaFinal.xaml.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Gymly.BD;
+using Gymly.Models;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Gymly.Models;
-using Gymly.BD;
-using Xceed.Wpf.Toolkit;
 namespace Gymly.UserControls
 {
     /// <summary>
