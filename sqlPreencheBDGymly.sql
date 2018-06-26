407a4daf0af8342f753bac8b1713c6c82f302348
Felipe Levez <34388096+levezf@users.noreply.github.com>
2018-06-25 21:59:03 -0300

    Add files via upload

---
+++ sqlPreencheBDGymly.sql
@@ -0,0 +1,40 @@
+INSERT INTO AVALIADORES(NOME)VALUES('Felipe Levez');
+INSERT INTO AVALIADORES(NOME)VALUES('Antonio Duarte');
+INSERT INTO AVALIADORES(NOME)VALUES('Washington Souza');
+INSERT INTO AVALIADORES(NOME)VALUES('Lincoln Abraham');
+INSERT INTO AVALIADORES(NOME)VALUES('Deodoro da Fonseca');
+INSERT INTO AVALIADORES(NOME)VALUES('Floriano Peixoto');
+INSERT INTO AVALIADORES(NOME)VALUES('Campos Salles');
+INSERT INTO AVALIADORES(NOME)VALUES('Rodrigues Alves');
+INSERT INTO AVALIADORES(NOME)VALUES('Hermes da Fonseca');
+INSERT INTO AVALIADORES(NOME)VALUES('Wenceslau Braz');
+INSERT INTO AVALIADORES(NOME)VALUES('Epitácio Pessoa');
+
+
+
+INSERT INTO ALUNOS(cpf, nome, datanasc, email, telefone,sexo, nivel, foto_de_rosto) 
+	VALUES ('295.170.384-88', 'Júlio Prestes', '1999-04-23','juju@gmail.com' ,'(16)3445-9876', 'M', 'Sedentário', '\Fotos\29517038488\Cadastro\rosto.jpg');
+INSERT INTO ALUNOS(cpf, nome, datanasc, email, telefone,sexo, nivel, foto_de_rosto) 
+	VALUES ('124.590.867-76', 'Delfim Moreira', '1986-06-19' ,'delfimomo@gmail.com' , '(16)3413-5976', 'M', 'Avançado', '\Fotos\12459086776\Cadastro\rosto.jpg');
+INSERT INTO ALUNOS(cpf, nome, datanasc, email, telefone,sexo, nivel, foto_de_rosto) 
+	VALUES ('709.685.234-47', 'Wenceslau Braz', '1998-05-03','brazwen@gmail.com', '(16)3416-4569', 'M', 'Iniciante', '\Fotos\70968523447\Cadastro\rosto.jpg');
+INSERT INTO ALUNOS(cpf, nome, datanasc, email, telefone,sexo, nivel, foto_de_rosto) 
+	VALUES ('304.651.978-84', 'Arthur Bernardes', '1980-01-02','berthur@gmail.com', '(16)3371-7233', 'M', 'Intermediário', '\Fotos\30465197884\Cadastro\rosto.jpg');
+INSERT INTO ALUNOS(cpf, nome, datanasc, email, telefone,sexo, nivel, foto_de_rosto) 
+	VALUES ('538.264.179-09', 'Getúlio Vargas', '1977-04-27','gvagas@gmail.com', '(16)3371-9865', 'M', 'Intermediário', '\Fotos\53826417909\Cadastro\rosto.jpg');
+INSERT INTO ALUNOS(cpf, nome, datanasc, email, telefone,sexo, nivel, foto_de_rosto) 
+	VALUES ('071.542.396-70', 'Café Filho', '2000-09-30','chapai@gmail.com', '(16)3346-1600', 'M', 'Iniciante', '\Fotos\07154239670\Cadastro\rosto.jpg');
+INSERT INTO ALUNOS(cpf, nome, datanasc, email, telefone,sexo, nivel, foto_de_rosto) 
+	VALUES ('983.502.614-98', 'Carlos Luz', '1995-02-19','carlosescuro@gmail.com', '(16)3456-1800', 'M', 'Avançado', '\Fotos\98350261498\Cadastro\rosto.jpg');
+INSERT INTO ALUNOS(cpf, nome, datanasc, email, telefone,sexo, nivel, foto_de_rosto) 
+	VALUES ('097.812.345-04', 'Nereu Ramos', '1992-10-22','nereur@gmail.com', '(16)3382-1900', 'M', 'Intermediário', '\Fotos\09781234504\Cadastro\rosto.jpg');
+INSERT INTO ALUNOS(cpf, nome, datanasc, email, telefone,sexo, nivel, foto_de_rosto) 
+	VALUES ('986.157.430-10', 'Juscelino Kubitschek', '1979-12-18','jkplus@gmail.com', '(16)3375-7655', 'M', 'Iniciante', '\Fotos\98615743010\Cadastro\rosto.jpg');
+INSERT INTO ALUNOS(cpf, nome, datanasc, email, telefone,sexo, nivel, foto_de_rosto) 
+	VALUES ('982.746.530-92', 'Ranieri Mazzilli ', '1978-01-02','mazzilliranieri@gmail.com', '(16)3372-7133', 'M', 'Sedentário', '\Fotos\98274653092\Cadastro\rosto.jpg');
+INSERT INTO ALUNOS(cpf, nome, datanasc, email, telefone,sexo, nivel, foto_de_rosto) 
+	VALUES ('867.291.430-96', 'Keyla Church', '1981-07-05','keylachu@gmail.com', '(16)3372-7133', 'F', 'Intermediário', '\Fotos\86729143096\Cadastro\rosto.jpg');
+
+
+
+INSERT INTO ANAMNESES VALUES (1,1,1,0,0,0,1,0,1,1,1,1,0,0,0,1,1,0,0,0,1,1,0,1,0,1,1,1,1,1,1,0,0,0,1,1);
